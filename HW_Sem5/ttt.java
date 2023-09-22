81663121f60f1cadc4f24a7d27619066825e3c27
TimKSan <106276486+TimKSan@users.noreply.github.com>
2023-09-22 17:18:28 +0300

    Update ttt.java

--- HW_Sem5/ttt.java
+++ HW_Sem5/ttt.java
@@ -1,35 +1,5 @@
 package HW_Sem5;
 
-// import java.util.*;
-
-// public class ttt {
-//     public static void main(String[] args) {
-//         HashMap<String, ArrayList<String>> phoneBook = new HashMap<>();
-        
-//         addContact("Petrov", "123-456-7890", phoneBook);
-//         addContact("Ivanov", "222-333-4444", phoneBook);
-//         addContact("Ivanov", "555-654-3210", phoneBook);
-
-//         for (Map.Entry<String, ArrayList<String>> entry : phoneBook.entrySet()) {
-//             int count = entry.getValue().size();
-//             if (count > 0) {
-//                 phoneBook.put(entry.getKey(), new ArrayList<>(count));
-//             }
-//         }
-//         System.out.println("Phone Book:");
-//         phoneBook.forEach((k, v) -> {
-//             System.out.printf("%s: %s%n", k, String.join(" ", v));
-//         });
-//     }
-
-//     private static void addContact(String name, String phone, HashMap<String, ArrayList<String>>phoneBook) {
-//         ArrayList<String> phones = phoneBook.get(name);
-
-//         phones.add(phone);
-//         phoneBook.put(name, phones);
-//     }
-// }
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -56,3 +26,6 @@ public class ttt {
         phones.add(phone);
     }
 }
+
+//Petrov: [123-456-7890]
+//Ivanov: [222-333-4444, 555-654-3210]
